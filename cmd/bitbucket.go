dbb7fbf3cfa03b99184a32e4ed825a9aa8a2e899
Marco <macatmail@gmail.com>
2018-04-27 11:36:44 +0200

    [#156006676] WIP add single bitbucket repos crawler command.

---
+++ cmd/bitbucket.go
@@ -0,0 +1,110 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/italia/developers-italia-backend/crawler"
+	"github.com/italia/developers-italia-backend/httpclient"
+	metrics "github.com/italia/developers-italia-backend/metrics"
+	"github.com/prometheus/client_golang/prometheus"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(bitbucketCmd)
+}
+
+var bitbucketCmd = &cobra.Command{
+	Use:   "bitbucket",
+	Short: "Crawl publiccode.yml from bitbucket.",
+	Long: `Start the crawler on bitbucket host defined on hosting.yml file.
+Beware! May take days to complete.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Init Prometheus for metrics.
+		processedCounter := metrics.PrometheusCounter("repository_processed_bitbucket", "Number of repository processed on Bitbucket.")
+
+		// Open and read hosting file list.
+		hostingFile := "hosting.yml"
+		data, err := ioutil.ReadFile(hostingFile)
+		if err != nil {
+			panic(fmt.Sprintf("error in reading %s file: %v", hostingFile, err))
+		}
+		// Parse hosting file list.
+		hostings, err := crawler.ParseHostingFile(data)
+		if err != nil {
+			panic(fmt.Sprintf("error in parsing %s file: %v", hostingFile, err))
+		}
+		log.Debug("Loaded and parsed hosting.yml")
+
+		// Initiate a channel of repositories.
+		repositories := make(chan crawler.Repository, 100)
+
+		// For each host parsed from hosting, Process the repositories.
+		for _, hosting := range hostings {
+			if hosting.ServiceName == "bitbucket" {
+				go crawler.Process(hosting, repositories)
+			}
+		}
+
+		// Process the repositories in order to retrieve publiccode.yml.
+		processRepositoriesBitbucket(repositories, processedCounter)
+	},
+}
+
+func processRepositoriesBitbucket(repositories chan crawler.Repository, processedCounter prometheus.Counter) {
+	log.Debug("Repositories are going to be processed...")
+	// Throttle requests.
+	// Time limits should be calibrated on more tests in order to avoid errors and bans.
+	// 1/100 can perform a number of request < bitbucket limit.
+	throttleRate := time.Second / 100
+	throttle := time.Tick(throttleRate)
+
+	for repository := range repositories {
+		// Throttle down the calls.
+		<-throttle
+		go checkAvailabilityBitbucket(repository.Name, repository.URL, repository.Headers, processedCounter)
+
+	}
+
+}
+
+func checkAvailabilityBitbucket(fullName, url string, headers map[string]string, processedCounter prometheus.Counter) {
+	processedCounter.Inc()
+
+	body, status, _, err := httpclient.GetURL(url, headers)
+	// If it's available and no error returned.
+	if status.StatusCode == http.StatusOK && err == nil {
+		// Save the file.
+		vendor, repo := splitFullNameGithub(fullName)
+		fileName := os.Getenv("CRAWLED_FILENAME")
+		saveFileBitbucket(vendor, repo, fileName, body)
+	}
+}
+
+// saveFile save the choosen <file_name> in ./data/<vendor>/<repo>/<file_name>
+func saveFileBitbucket(vendor, repo, fileName string, data []byte) {
+	path := filepath.Join("./data", "bitbucket.com", vendor, repo)
+
+	// MkdirAll will create all the folder path, if not exists.
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		os.MkdirAll(path, os.ModePerm)
+	}
+
+	err := ioutil.WriteFile(path+"/"+fileName, data, 0644)
+	if err != nil {
+		log.Error(err)
+	}
+}
+
+// splitFullName split a git FullName format to vendor and repo strings.
+func splitFullNameBitbucket(fullName string) (string, string) {
+	s := strings.Split(fullName, "/")
+	return s[0], s[1]
+}
--- crawler/bitbucket.go
+++ crawler/bitbucket.go
@@ -3,8 +3,10 @@ package crawler
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/italia/developers-italia-backend/httpclient"
 	log "github.com/sirupsen/logrus"
@@ -20,17 +22,122 @@ type Bitbucket struct {
 	BasicAuth string `yaml:"basic-auth"`
 }
 
-type bitbucketResponse struct {
-	Values []struct {
-		Name  string `json:"name"`
-		Links struct {
+type response struct {
+	Pagelen int `json:"pagelen"`
+	Values  []struct {
+		Scm     string `json:"scm"`
+		Website string `json:"website"`
+		HasWiki bool   `json:"has_wiki"`
+		Name    string `json:"name"`
+		Links   struct {
+			Watchers struct {
+				Href string `json:"href"`
+			} `json:"watchers"`
+			Branches struct {
+				Href string `json:"href"`
+			} `json:"branches"`
+			Tags struct {
+				Href string `json:"href"`
+			} `json:"tags"`
+			Commits struct {
+				Href string `json:"href"`
+			} `json:"commits"`
 			Clone []struct {
 				Href string `json:"href"`
 				Name string `json:"name"`
 			} `json:"clone"`
+			Self struct {
+				Href string `json:"href"`
+			} `json:"self"`
+			Source struct {
+				Href string `json:"href"`
+			} `json:"source"`
+			HTML struct {
+				Href string `json:"href"`
+			} `json:"html"`
+			Avatar struct {
+				Href string `json:"href"`
+			} `json:"avatar"`
+			Hooks struct {
+				Href string `json:"href"`
+			} `json:"hooks"`
+			Forks struct {
+				Href string `json:"href"`
+			} `json:"forks"`
+			Downloads struct {
+				Href string `json:"href"`
+			} `json:"downloads"`
+			Pullrequests struct {
+				Href string `json:"href"`
+			} `json:"pullrequests"`
 		} `json:"links"`
-		FullName    string `json:"full_name"`
-		Description string `json:"description"`
+		ForkPolicy string    `json:"fork_policy"`
+		UUID       string    `json:"uuid"`
+		Language   string    `json:"language"`
+		CreatedOn  time.Time `json:"created_on"`
+		Mainbranch struct {
+			Type string `json:"type"`
+			Name string `json:"name"`
+		} `json:"mainbranch"`
+		FullName  string `json:"full_name"`
+		HasIssues bool   `json:"has_issues"`
+		Owner     struct {
+			Username    string `json:"username"`
+			DisplayName string `json:"display_name"`
+			Type        string `json:"type"`
+			UUID        string `json:"uuid"`
+			Links       struct {
+				Self struct {
+					Href string `json:"href"`
+				} `json:"self"`
+				HTML struct {
+					Href string `json:"href"`
+				} `json:"html"`
+				Avatar struct {
+					Href string `json:"href"`
+				} `json:"avatar"`
+			} `json:"links"`
+		} `json:"owner"`
+		UpdatedOn   time.Time `json:"updated_on"`
+		Size        int       `json:"size"`
+		Type        string    `json:"type"`
+		Slug        string    `json:"slug"`
+		IsPrivate   bool      `json:"is_private"`
+		Description string    `json:"description"`
+		Project     struct {
+			Key   string `json:"key"`
+			Type  string `json:"type"`
+			UUID  string `json:"uuid"`
+			Links struct {
+				Self struct {
+					Href string `json:"href"`
+				} `json:"self"`
+				HTML struct {
+					Href string `json:"href"`
+				} `json:"html"`
+				Avatar struct {
+					Href string `json:"href"`
+				} `json:"avatar"`
+			} `json:"links"`
+			Name string `json:"name"`
+		} `json:"project,omitempty"`
+		Parent struct {
+			Links struct {
+				Self struct {
+					Href string `json:"href"`
+				} `json:"self"`
+				HTML struct {
+					Href string `json:"href"`
+				} `json:"html"`
+				Avatar struct {
+					Href string `json:"href"`
+				} `json:"avatar"`
+			} `json:"links"`
+			Type     string `json:"type"`
+			Name     string `json:"name"`
+			FullName string `json:"full_name"`
+			UUID     string `json:"uuid"`
+		} `json:"parent,omitempty"`
 	} `json:"values"`
 	Next string `json:"next"`
 }
@@ -38,6 +145,7 @@ type bitbucketResponse struct {
 // GetRepositories retrieves the list of all repository from an hosting.
 // Return the URL from where it should restart (Next or actual if fails) and error.
 func (host Bitbucket) GetRepositories(url string, repositories chan Repository) (string, error) {
+	fmt.Println("-> GetRepositories: " + url)
 	// Set BasicAuth header
 	headers := make(map[string]string)
 	if host.BasicAuth != "" {
@@ -55,7 +163,7 @@ func (host Bitbucket) GetRepositories(url string, repositories chan Repository) 
 	}
 
 	// Fill response as list of values (repositories data).
-	var result bitbucketResponse
+	var result response
 	err = json.Unmarshal(body, &result)
 	if err != nil {
 		return url, err
@@ -63,19 +171,27 @@ func (host Bitbucket) GetRepositories(url string, repositories chan Repository) 
 
 	// Add repositories to the channel that will perform the check on everyone.
 	for _, v := range result.Values {
-		repositories <- Repository{
-			Name:    v.FullName,
-			URL:     v.Links.Clone[0].Href + "/raw/default/" + os.Getenv("CRAWLED_FILENAME"),
-			Source:  url,
-			Headers: headers,
+		// If the repository was never used, the mainbrench name is empty ("")
+		if v.Mainbranch.Name != "" {
+			repositories <- Repository{
+				Name:    v.FullName,
+				URL:     v.Links.HTML.Href + "/raw/" + v.Mainbranch.Name + "/" + os.Getenv("CRAWLED_FILENAME"),
+				Source:  url,
+				Headers: headers,
+			}
+
 		}
+
 	}
 
+	// Bitbucket end reached.
 	if len(result.Next) == 0 {
 		// If I want to restart when it ends:
 		// sourceURL = "https://api.bitbucket.org/2.0/repositories?pagelen=100&after=2008-08-13"
 		// and comment the line "close(repositories)"
-		log.Info("Bitbucket repositories status: end reached.")
+		for len(repositories) != 0 {
+			time.Sleep(time.Second)
+		}
 		close(repositories)
 		return url, nil
 	}
