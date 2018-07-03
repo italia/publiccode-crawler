2d0dce37af8339e37ae6d447195cfa9a2db0b844
Marco <macatmail@gmail.com>
2018-07-03 10:49:42 +0200

    [#157926180] WIP Fix amministrazioni.yml with amministrazioni.txt data.

--- jekyll/amministrazioni.go
+++ jekyll/amministrazioni.go
@@ -1,9 +1,12 @@
 package jekyll
 
 import (
+	"bufio"
 	"context"
+	"io/ioutil"
 	"os"
 	"reflect"
+	"strings"
 
 	"github.com/italia/developers-italia-backend/crawler"
 	"github.com/olivere/elastic"
@@ -65,17 +68,19 @@ func AmministrazioniYML(filename string, elasticClient *elastic.Client) error {
 		i := item.(crawler.PublicCodeES)
 		if i.ItRiusoCodiceIPA != "" {
 			administrations = append(administrations, Administration{
-				Name:      i.Name,
+				Name:      getNomeAmministrazione(i.ItRiusoCodiceIPA),
 				URL:       i.URL,
 				CodiceIPA: i.ItRiusoCodiceIPA,
 			})
-
 		}
 	}
 	// Debug note if file will be empty.
 	if len(administrations) == 0 {
 		log.Debug("amministrazioni.yml is empty.")
 	}
+
+	// Remove duplicates.
+	administrations = removeDuplicates(administrations)
 
 	// Marshal yml.
 	d, err := yaml.Marshal(&administrations)
@@ -88,4 +93,118 @@ func AmministrazioniYML(filename string, elasticClient *elastic.Client) error {
 	}
 
 	return err
+}
+
+func removeDuplicates(elements []Administration) []Administration {
+	// Use map to record duplicates as we find them.
+	encountered := map[string]bool{}
+	result := []Administration{}
+
+	for v := range elements {
+		if encountered[elements[v].CodiceIPA] == true {
+			// Do not add duplicate.
+		} else {
+			// Record this element as an encountered element.
+			encountered[elements[v].CodiceIPA] = true
+			// Append to result slice.
+			result = append(result, elements[v])
+		}
+	}
+	// Return the new slice.
+	return result
+}
+
+type Amministrazione struct {
+	cod_amm            string
+	des_amm            string
+	Comune             string
+	nome_resp          string
+	cogn_resp          string
+	Cap                string
+	Provincia          string
+	Regione            string
+	sito_istituzionale string
+	Indirizzo          string
+	titolo_resp        string
+	tipologia_istat    string
+	tipologia_amm      string
+	acronimo           string
+	cf_validato        string
+	Cf                 string
+	mail1              string
+	tipo_mail1         string
+	mail2              string
+	tipo_mail2         string
+	mail3              string
+	tipo_mail3         string
+	mail4              string
+	tipo_mail4         string
+	mail5              string
+	tipo_mail5         string
+	url_facebook       string
+	url_twitter        string
+	url_googleplus     string
+	url_youtube        string
+	liv_accessibili    string
+}
+
+func getNomeAmministrazione(codiceiPA string) string {
+	file := "jekyll/amministrazioni.txt"
+	dataFile, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Error(err)
+		return ""
+	}
+	input := string(dataFile)
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	for scanner.Scan() {
+		amm := manageLine(scanner.Text())
+		if amm.cod_amm == codiceiPA {
+			return amm.des_amm
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Errorf("error reading standard input %v:", err)
+	}
+
+	return ""
+}
+
+func manageLine(line string) Amministrazione {
+	data := strings.Split(line, "	")
+	amm := Amministrazione{
+		cod_amm:            data[0],
+		des_amm:            data[1],
+		Comune:             data[2],
+		nome_resp:          data[3],
+		cogn_resp:          data[4],
+		Cap:                data[5],
+		Provincia:          data[6],
+		Regione:            data[7],
+		sito_istituzionale: data[8],
+		Indirizzo:          data[9],
+		titolo_resp:        data[10],
+		tipologia_istat:    data[11],
+		tipologia_amm:      data[12],
+		acronimo:           data[13],
+		cf_validato:        data[14],
+		Cf:                 data[15],
+		mail1:              data[16],
+		tipo_mail1:         data[17],
+		mail2:              data[18],
+		tipo_mail2:         data[19],
+		mail3:              data[20],
+		tipo_mail3:         data[21],
+		mail4:              data[22],
+		tipo_mail4:         data[23],
+		mail5:              data[24],
+		tipo_mail5:         data[25],
+		url_facebook:       data[26],
+		url_twitter:        data[27],
+		url_googleplus:     data[28],
+		url_youtube:        data[29],
+		liv_accessibili:    data[30],
+	}
+
+	return amm
 }
