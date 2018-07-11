5952cd3cbba364d562b4f8513e2ed917833e4232
Marco <macatmail@gmail.com>
2018-07-11 09:19:19 +0200

    [#158836814] Fix gometalinter.

--- ipa/amministrazioni.go
+++ ipa/amministrazioni.go
@@ -16,37 +16,37 @@ import (
 // Amministrazione is an Administration from amministrazoni.txt
 // Retrieved from: http://www.indicepa.gov.it/documentale/n-opendata.php
 type Amministrazione struct {
-	codAmm            string
-	desAmm            string
+	CodAmm            string
+	DesAmm            string
 	Comune            string
-	nomeResp          string
-	cognResp          string
+	NomeResp          string
+	CognResp          string
 	Cap               string
 	Provincia         string
 	Regione           string
-	sitoIstituzionale string
+	SitoIstituzionale string
 	Indirizzo         string
-	titoloResp        string
-	tipologiaIstat    string
-	tipologiaAmm      string
-	acronimo          string
-	cfValidato        string
-	Cf                string
-	mail1             string
-	tipoMail1         string
-	mail2             string
-	tipoMail2         string
-	mail3             string
-	tipoMail3         string
-	mail4             string
-	tipoMail4         string
-	mail5             string
-	tipoMail5         string
-	urlFacebook       string
-	urlTwitter        string
-	urlGoogleplus     string
-	urlYoutube        string
-	livAccessibili    string
+	TitoloResp        string
+	TipologiaIstat    string
+	TipologiaAmm      string
+	Acronimo          string
+	CFValidato        string
+	CF                string
+	Mail1             string
+	TipoMail1         string
+	Mail2             string
+	TipoMail2         string
+	Mail3             string
+	TipoMail3         string
+	Mail4             string
+	TipoMail4         string
+	Mail5             string
+	TipoMail5         string
+	URLFacebook       string
+	URLTwitter        string
+	URLGoogleplus     string
+	URLYoutube        string
+	LivAccessibili    string
 }
 
 // UpdateFile download the amministrazioni.txt file if it's older than 2 days.
@@ -60,7 +60,7 @@ func UpdateFile(fileName, fileURL string) error {
 
 	downloadTime := info.ModTime()
 
-	// If amministrazioni.txt is older that 2 days
+	// If amministrazioni.txt is older that 2 days.
 	if downloadTime.Before(older) {
 		fmt.Println("download a new amministrazioni.txt ...")
 
@@ -82,11 +82,13 @@ func GetAdministrationName(codiceiPA string) string {
 		return ""
 	}
 	input := string(dataFile)
+
+	// Scan the file, line by line.
 	scanner := bufio.NewScanner(strings.NewReader(input))
 	for scanner.Scan() {
 		amm := manageLine(scanner.Text())
-		if amm.codAmm == codiceiPA {
-			return amm.desAmm
+		if amm.CodAmm == codiceiPA {
+			return amm.DesAmm
 		}
 	}
 	if err := scanner.Err(); err != nil {
@@ -96,40 +98,41 @@ func GetAdministrationName(codiceiPA string) string {
 	return ""
 }
 
+// manageLine populate an Amministrazione with the values read.
 func manageLine(line string) Amministrazione {
 	data := strings.Split(line, "	")
 	amm := Amministrazione{
-		codAmm:            data[0],
-		desAmm:            data[1],
+		CodAmm:            data[0],
+		DesAmm:            data[1],
 		Comune:            data[2],
-		nomeResp:          data[3],
-		cognResp:          data[4],
+		NomeResp:          data[3],
+		CognResp:          data[4],
 		Cap:               data[5],
 		Provincia:         data[6],
 		Regione:           data[7],
-		sitoIstituzionale: data[8],
+		SitoIstituzionale: data[8],
 		Indirizzo:         data[9],
-		titoloResp:        data[10],
-		tipologiaIstat:    data[11],
-		tipologiaAmm:      data[12],
-		acronimo:          data[13],
-		cfValidato:        data[14],
-		Cf:                data[15],
-		mail1:             data[16],
-		tipoMail1:         data[17],
-		mail2:             data[18],
-		tipoMail2:         data[19],
-		mail3:             data[20],
-		tipoMail3:         data[21],
-		mail4:             data[22],
-		tipoMail4:         data[23],
-		mail5:             data[24],
-		tipoMail5:         data[25],
-		urlFacebook:       data[26],
-		urlTwitter:        data[27],
-		urlGoogleplus:     data[28],
-		urlYoutube:        data[29],
-		livAccessibili:    data[30],
+		TitoloResp:        data[10],
+		TipologiaIstat:    data[11],
+		TipologiaAmm:      data[12],
+		Acronimo:          data[13],
+		CFValidato:        data[14],
+		CF:                data[15],
+		Mail1:             data[16],
+		TipoMail1:         data[17],
+		Mail2:             data[18],
+		TipoMail2:         data[19],
+		Mail3:             data[20],
+		TipoMail3:         data[21],
+		Mail4:             data[22],
+		TipoMail4:         data[23],
+		Mail5:             data[24],
+		TipoMail5:         data[25],
+		URLFacebook:       data[26],
+		URLTwitter:        data[27],
+		URLGoogleplus:     data[28],
+		URLYoutube:        data[29],
+		LivAccessibili:    data[30],
 	}
 
 	return amm
@@ -142,20 +145,25 @@ func downloadFile(filepath string, url string) error {
 	if err != nil {
 		return err
 	}
-	defer out.Close()
+	defer func() {
+		if err := out.Close(); err != nil {
+			log.Error(err)
+		}
+	}()
 
-	// Get the data
+	// Get the data from the url.
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.Error(err)
+		}
+	}()
 
-	// Write the body to file
+	// Write the body to file.
 	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		return err
-	}
 
-	return nil
+	return err
 }
