dfd54e235ce50fc3ce7209b1daffcfd1eef6a6c7
Luca Prete <lucaprete@teamdigitale.governo.it>
2020-03-17 10:12:29 +0100

    [#129] Add Docker support for Devita crawler (#130)

---
+++ crawler/start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Sometimes, Elasticsearch is still not ready, yet.
+# Let's wait 30 seconds to be sure Elasticsearch is ready
+# to accept connections
+time=30
+
+echo "${0##*/}: Waiting ${time} seconds before running the crawler..."
+
+sleep ${time}
+
+bin/crawler updateipa
+bin/crawler download-whitelist https://onboarding.developers.italia.it/repo-list whitelist/onboarding-reuse.yml
+bin/crawler crawl whitelist/*.yml
