85df5f70936c91919c0253be22c925642f973631
Fabio Bonelli <fb@fabiobonelli.it>
2022-10-14 16:26:28 +0200

    chore: remove unused files

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl 'https://www.indicepa.gov.it/public-services/opendata-read-service.php?dstype=FS&filename=pec.txt' > pec.txt
-
-bin/crawler download-publishers https://onboarding.developers.italia.it/repo-list publishers.onboarding.yml
-bin/crawler crawl publishers*.yml
