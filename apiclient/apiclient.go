italia/publiccode-crawler

commit 0e4e25c014057b47c477111c411815251ea79e23

refactor: enable prealloc linter (#345)

apiclient/apiclient.go
@@ -123,7 +123,6 @@ func (clt ApiClient) Patch(url string, body []byte) (*http.Response, error) {
 // any error encountered.
 func (c ApiClient) GetPublishers() ([]common.Publisher, error) {
 	var publishersResponse *PublishersPaginated
-	var publishers []common.Publisher
 
 	pageAfter := ""
 
@@ -147,6 +146,7 @@ page:
 		return nil, fmt.Errorf("can't parse GET %s response: %w", reqUrl, err)
 	}
 
+	publishers := make([]common.Publisher, 0, len(publishersResponse.Data))
 	for _, p := range publishersResponse.Data {
 		var groups, repos []internalUrl.URL
 

.golangci.yaml
@@ -129,7 +129,7 @@ linters:
     - nonamedreturns
     - nosprintfhostport
     - paralleltest
-    # - prealloc
+    - prealloc
     - predeclared
     - promlinter
     - reassign

commit 986f8fb58fc0213f72f2e220bf5f46aaf07c51fb

fix: properly get all the Publishers
Fix the bug introduced in 0e4e25c01405 that returned only the first
10 Publishers.

apiclient/apiclient.go
@@ -125,6 +125,7 @@ func (c ApiClient) GetPublishers() ([]common.Publisher, error) {
 	var publishersResponse *PublishersPaginated
 
 	pageAfter := ""
+	publishers := make([]common.Publisher, 0, 25)
 
 page:
 	reqUrl := joinPath(c.baseURL, "/publishers") + pageAfter
@@ -146,7 +147,6 @@ page:
 		return nil, fmt.Errorf("can't parse GET %s response: %w", reqUrl, err)
 	}
 
-	publishers := make([]common.Publisher, 0, len(publishersResponse.Data))
 	for _, p := range publishersResponse.Data {
 		var groups, repos []internalUrl.URL
 