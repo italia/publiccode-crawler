863f7f66f8fd5b516a42b9c17af271ba12c524ca
Fabio Bonelli <fb@fabiobonelli.it>
2023-07-15 14:20:28 +0200

    feat: get publishers files on run (#358)
    Get publishers on run instead of in the build of the Docker image,
    so every run is up to date with the current publishers.

--- start.sh
+++ start.sh
@@ -2,4 +2,7 @@
 
 bin/crawler updateipa
 bin/crawler download-publishers https://onboarding.developers.italia.it/repo-list publishers.onboarding.yml
+wget https://raw.githubusercontent.com/italia/developers.italia.it/HEAD/_data/publishers.thirdparty.yml
+wget https://raw.githubusercontent.com/italia/developers.italia.it/HEAD/_data/publishers.yml
+
 bin/crawler crawl publishers*.yml
